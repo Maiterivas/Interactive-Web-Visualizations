9cf3d0f02ab536f6759cc44839acc8f6ed2f69df
Maiterivas <maiterivas09@gmail.com>
2022-10-05 17:42:54 -0500

    updating selections

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,64 @@
-const url = 
+//locate where the data is
+const url = "data/samples.json"
 
-function metadata_info(selected_value) {
-    d3.json(url+selected_value).then((data) => {
-        var metadata= data.metadata;
-        var array = metadata.filter(sample => sample.id == sample_value);
+//testing getting data
+d3.json(url).then((data) => {
+metadata = data;
+console.log(metadata);
+
+});
+
+
+function optionChanged(sample_value) {
+    buildBarChart(sample_value);
+    buildBubbleChart(sample_value);
+    metadata_info(sample_value);
+}
+
+//function will initialize value selection
+function init() {
+  // get the data from samples.json and add names as value options for visualizations
+  d3.json(url).then((data) => {
+    var all_samples = data.samples
+    // save the html id to a variable to update it within
+    var sel_dataset = d3.select("#selDataset");
+  
+    //loop through saved samples so it can build charts
+    for (i = 0; i < all_samples.length; i++) {
+      var sample_update = 
+      sel_dataset
+      .append("option")
+      //specify i value to use from loop
+      .text(`${all_samples[i].id}`);
+      // update current value of sample
+      sample_update.property("value", all_samples[i].id);
+    }
+      
         
-        // select sample metadata using id tag in index html
-        var selectvalue = d3.select("#sample-metadata");
-        // .html("") clears any previous metadata info
-        selectvalue.html("");
-        Object.entries(result).forEach(([a, b]) => {
-        selectsample.append("h5").text(`${a}: ${b}`);
-      });
-    });
+  });
+  
+      //initialize charts with first ID sample value
+      buildBarChart(all_samples[0].id);
+      buildBubbleChart(all_samples[0].id);
+      metadata_info(all_samples[0].id);
+};
+  
+  
+//initialize init function by calling it
+init();
+
+function metadata_info(sample_value) {
+  d3.json(url).then((data) => {
+    //save metadata
+    var metadata = data.metadata;
+    //save selections for each subject
+    var sample_select = metadata.filter(value => value.id == sample_value);
+    var sample_result = sample_select[0]
+    var sample_metadata = d3.select("#sample-metadata");
+    //clear out previous entries
+    sample_metadata.html("");
+    Object.entries(sample_result).forEach(([key, pair]) => {
+    sample_metadata.append("h5").text(`${key}: ${pair}`);
+  });
+});
 }
